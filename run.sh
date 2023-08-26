cac85de0f2ef1b4f8f756ca56b589a3f8a989341
zelin_nus_home_202 <zangzelin@gmail.com>
2023-08-26 11:54:13 +0000

    syn

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@ eval "$(/zangzelin/conda/bin/conda shell.bash hook)"; conda activate env_san
 CUDA_VISIBLE_DEVICES=0 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
 CUDA_VISIBLE_DEVICES=1 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
 CUDA_VISIBLE_DEVICES=2 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
-CUDA_VISIBLE_DEVICES=3 wandb agent zangzelin/OVANET_DMT/yu5k29s9 
+CUDA_VISIBLE_DEVICES=2 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
+CUDA_VISIBLE_DEVICES=3 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
+CUDA_VISIBLE_DEVICES=4 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
+CUDA_VISIBLE_DEVICES=5 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
+CUDA_VISIBLE_DEVICES=7 wandb agent zangzelin/OVANET_DMT/yu5k29s9 &
+CUDA_VISIBLE_DEVICES=6 wandb agent zangzelin/OVANET_DMT/yu5k29s9 
