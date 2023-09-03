f5588e95f068ab090e6607ac1478572088e3f5e0
zangzelindocker12 <zangzelin>
2023-09-03 04:00:54 +0000

    auto push

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@
 eval "$(/zangzelin/conda/bin/conda shell.bash hook)"; conda activate env_san
 wandb login --host=https://api.wandb.ai 46a45f51484e3cce8ee9d0d270ea18b98ef9151d
 
-CUDA_VISIBLE_DEVICES=0 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 0-7 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=0 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 0-7 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=1 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 8-15 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=1 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 8-15 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=2 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 16-23 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=2 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 16-23 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=3 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 24-31 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=3 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 24-31 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=4 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 32-39 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=4 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 32-39 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=5 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 40-47 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=5 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 40-47 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=7 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 48-55 zangzelin_hotmail/OVANET_DMT/n0k1s38j &
+CUDA_VISIBLE_DEVICES=7 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 48-55 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j &
 sleep 60
-CUDA_VISIBLE_DEVICES=6 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 56-63 zangzelin_hotmail/OVANET_DMT/n0k1s38j 
+CUDA_VISIBLE_DEVICES=6 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 56-63 wandb agent zangzelin_hotmail/OVANET_DMT/n0k1s38j 
