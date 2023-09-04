ec0002fc3352d623c01fc3cc91bd1b3c4b265b46
zangzelindocker12 <zangzelin>
2023-09-04 02:31:25 +0000

    auto push

--- run_4.sh
+++ run_4.sh
@@ -1,6 +1,6 @@
 
 eval "$(/zangzelin/conda/bin/conda shell.bash hook)"; conda activate env_san
-wandb login --host=https://api.wandb.ai 46a45f51484e3cce8ee9d0d270ea18b98ef9151d
+wandb login --relogin --host=https://api.wandb.ai 46a45f51484e3cce8ee9d0d270ea18b98ef9151d
 
 CUDA_VISIBLE_DEVICES=0 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 0-7  wandb agent zangzelin_hotmail/OVANET_DMT/5x4cddqw &
 CUDA_VISIBLE_DEVICES=1 CUBLAS_WORKSPACE_CONFIG=:4096:8 taskset -c 8-15  wandb agent zangzelin_hotmail/OVANET_DMT/5x4cddqw &
